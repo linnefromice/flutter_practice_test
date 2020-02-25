68ad73ad57637570311dceafd01432cabd792022
linnefromice <linnefromice@gmail.com>
2020-02-26 03:42:48 +0900

    add unit test

--- test/reverse_screen/unit_test_reverse.dart
+++ test/reverse_screen/unit_test_reverse.dart
@@ -2,9 +2,14 @@ import 'package:flutter_practice_test/screen/ReverseScreen.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('String should be reversed', () {
+  test('test 01 - String should be reversed', () {
     String initial = "Hello";
     String reversed = reverseString(initial);
     expect(reversed, 'olleH');
+  });
+  test('test 02 - String should be reversed', () {
+    String initial = "12345XYZ67890";
+    String reversed = reverseString(initial);
+    expect(reversed, '09876ZYX54321');
   });
 }
