4eab75182cb7f1ea35a6fc9f1fb7479ea2e850d3
linnefromice <linnefromice@gmail.com>
2020-02-26 03:33:06 +0900

    modify app for test

--- lib/screen/ReverseScreen.dart
+++ lib/screen/ReverseScreen.dart
@@ -6,11 +6,50 @@ class ReverseScreen extends StatefulWidget {
 }
 
 class _State extends State<ReverseScreen> {
+  TextEditingController _controller = TextEditingController();
+  String _reversed;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Reverse App')),
-      body: Text('Reverse App'),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextField(
+              controller: _controller,
+            ),
+            SizedBox(height: 10.0),
+            if (_reversed != null) ...[
+              Text(
+                _reversed,
+              ),
+              SizedBox(height: 10.0),
+            ],
+            RaisedButton(
+              child: Text('Reverse'),
+              onPressed: () {
+                if (_controller.text.isEmpty) return;
+                setState(() {
+                  _reversed = reverseString(_controller.text);
+                });
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
+}
+
+String reverseString(String initial) {
+  return initial.split('').reversed.join();
 }
